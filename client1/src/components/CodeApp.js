f958606bf848917e81f9ace28914ebb5dacb0f1f
SubhojitM46 <156497276+SubhojitM46@users.noreply.github.com>
2024-05-20 17:51:32 +0530

    Delete client1 directory

--- client1/src/components/CodeApp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import crudb from '../images/crudb.jpg'
-
-export default function CodeApp() {
-  return (
-    <center><img src={crudb} alt="banner" height="70%" width="80%" /></center>
-  )
-}
